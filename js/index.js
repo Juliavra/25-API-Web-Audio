a55c33ad82fc24573397ba93154a2ad73ca55278
Julian Avramides <julianavramides@hotmail.com>
2023-05-23 06:48:50 -0300

    1st commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+var contexto;
+function iniciar() {
+var mibuffer;
+var boton = document.getElementById("boton");
+boton.addEventListener("click", function() {
+reproducir(mibuffer);
+});
+contexto = new AudioContext();
+var url = "20 Dubby Augustus Pablo NR Song.mp3";
+var solicitud = new XMLHttpRequest(); alert("solicitud: "+`${solicitud}`)
+solicitud.responseType = "arraybuffer";
+solicitud.addEventListener("load", function() {
+if (solicitud.status == 200) {
+contexto.decodeAudioData(solicitud.response, function(buffer) {
+mibuffer = buffer;
+boton.disabled = false;
+});
+}
+});
+solicitud.open("GET", url, true);
+solicitud.send();
+}
+function reproducir(mibuffer) {
+var nodofuente = contexto.createBufferSource();
+nodofuente.buffer = mibuffer;
+nodofuente.connect(contexto.destination);
+nodofuente.start(0);
+}
+window.addEventListener("load", iniciar);
