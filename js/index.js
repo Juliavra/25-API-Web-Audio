2a67a4896bccab761a35f22cf55dbc82d67bb43b
Julian Avramides <julianavramides@hotmail.com>
2023-05-23 08:45:47 -0300

    7th commit trying to find correct path to audio file

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ boton.addEventListener("click", function() {
 reproducir(mibuffer);
 });
 contexto = new AudioContext();
-var url = "https://github.com/Juliavra/25-API-Web-Audio/audio/01 Dark Ringy Short Loop.mp3";
+var url = "https://github.com/Juliavra/25-API-Web-Audio/01 Dark Ringy Short Loop.mp3";
 var solicitud = new XMLHttpRequest(); alert("solicitud: "+`${solicitud}`)
 solicitud.responseType = "arraybuffer";
 solicitud.addEventListener("load", function() {
